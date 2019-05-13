bb2003ae8af7c5c51c92ee14a3ae8c21d8895f41
Alex Fargo <me@alexfargo.xyz>
2019-05-13 15:43:39 -0700

    Created helper python program for parsing categories and creating an enum

---
+++ Food Search/Supporing Files/Categories.swift
@@ -0,0 +1,593 @@
+//
+//  Categories.swift
+//  Food Search
+//
+//  Created by Alex Fargo on 5/13/19.
+//  Copyright © 2019 Alex Fargo. All rights reserved.
+//
+
+import Foundation
+
+enum Categories: String {
+	case afghani = "Afghan"
+	case african = "African"
+	case andalusian = "Andalusian"
+	case arabian = "Arabian"
+	case argentine = "Argentine"
+	case armenian = "Armenian"
+	case asianfusion = "Asian Fusion"
+	case asturian = "Asturian"
+	case australian = "Australian"
+	case austrian = "Austrian"
+	case baguettes = "Baguettes"
+	case bangladeshi = "Bangladeshi"
+	case basque = "Basque"
+	case bavarian = "Bavarian"
+	case bbq = "Barbeque"
+	case beergarden = "Beer Garden"
+	case beerhall = "Beer Hall"
+	case beisl = "Beisl"
+	case belgian = "Belgian"
+	case bistros = "Bistros"
+	case blacksea = "Black Sea"
+	case brasseries = "Brasseries"
+	case brazilian = "Brazilian"
+	case breakfast_brunch = "Breakfast & Brunch"
+	case british = "British"
+	case buffets = "Buffets"
+	case bulgarian = "Bulgarian"
+	case burgers = "Burgers"
+	case burmese = "Burmese"
+	case cafes = "Cafes"
+	case cafeteria = "Cafeteria"
+	case cajun = "Cajun/Creole"
+	case cambodian = "Cambodian"
+	case canteen = "Canteen"
+	case caribbean = "Caribbean"
+	case catalan = "Catalan"
+	case cheesesteaks = "Cheesesteaks"
+	case chicken_wings = "Chicken Wings"
+	case chickenshop = "Chicken Shop"
+	case chilean = "Chilean"
+	case chinese = "Chinese"
+	case comfortfood = "Comfort Food"
+	case corsican = "Corsican"
+	case creperies = "Creperies"
+	case cuban = "Cuban"
+	case currysausage = "Curry Sausage"
+	case cypriot = "Cypriot"
+	case czech = "Czech"
+	case czechslovakian = "Czech/Slovakian"
+	case danish = "Danish"
+	case delis = "Delis"
+	case diners = "Diners"
+	case dinnertheater = "Dinner Theater"
+	case dumplings = "Dumplings"
+	case eastern_european = "Eastern European"
+	case eltern_cafes = "Parent Cafes"
+	case eritrean = "Eritrean"
+	case ethiopian = "Ethiopian"
+	case filipino = "Filipino"
+	case fischbroetchen = "Fischbroetchen"
+	case fishnchips = "Fish & Chips"
+	case flatbread = "Flatbread"
+	case fondue = "Fondue"
+	case food_court = "Food Court"
+	case foodstands = "Food Stands"
+	case freiduria = "Freiduria"
+	case french = "French"
+	case galician = "Galician"
+	case gamemeat = "Game Meat"
+	case gastropubs = "Gastropubs"
+	case georgian = "Georgian"
+	case german = "German"
+	case giblets = "Giblets"
+	case gluten_free = "Gluten-Free"
+	case greek = "Greek"
+	case guamanian = "Guamanian"
+	case halal = "Halal"
+	case hawaiian = "Hawaiian"
+	case heuriger = "Heuriger"
+	case himalayan = "Himalayan/Nepalese"
+	case hkcafe = "Hong Kong Style Cafe"
+	case honduran = "Honduran"
+	case hotdog = "Hot Dogs"
+	case hotdogs = "Fast Food"
+	case hotpot = "Hot Pot"
+	case hungarian = "Hungarian"
+	case iberian = "Iberian"
+	case indonesian = "Indonesian"
+	case indpak = "Indian"
+	case international = "International"
+	case irish = "Irish"
+	case island_pub = "Island Pub"
+	case israeli = "Israeli"
+	case italian = "Italian"
+	case japanese = "Japanese"
+	case jewish = "Jewish"
+	case kebab = "Kebab"
+	case kopitiam = "Kopitiam"
+	case korean = "Korean"
+	case kosher = "Kosher"
+	case kurdish = "Kurdish"
+	case laos = "Laos"
+	case laotian = "Laotian"
+	case latin = "Latin American"
+	case lyonnais = "Lyonnais"
+	case malaysian = "Malaysian"
+	case meatballs = "Meatballs"
+	case mediterranean = "Mediterranean"
+	case mexican = "Mexican"
+	case mideastern = "Middle Eastern"
+	case milkbars = "Milk Bars"
+	case modern_australian = "Modern Australian"
+	case modern_european = "Modern European"
+	case mongolian = "Mongolian"
+	case moroccan = "Moroccan"
+	case newamerican = "American (New)"
+	case newcanadian = "Canadian (New)"
+	case newmexican = "New Mexican Cuisine"
+	case newzealand = "New Zealand"
+	case nicaraguan = "Nicaraguan"
+	case nightfood = "Night Food"
+	case nikkei = "Nikkei"
+	case noodles = "Noodles"
+	case norcinerie = "Norcinerie"
+	case norwegian = "Traditional Norwegian"
+	case opensandwiches = "Open Sandwiches"
+	case oriental = "Oriental"
+	case pakistani = "Pakistani"
+	case panasian = "Pan Asian"
+	case parma = "Parma"
+	case persian = "Persian/Iranian"
+	case peruvian = "Peruvian"
+	case pfcomercial = "PF/Comercial"
+	case pita = "Pita"
+	case pizza = "Pizza"
+	case polish = "Polish"
+	case polynesian = "Polynesian"
+	case popuprestaurants = "Pop-Up Restaurants"
+	case portuguese = "Portuguese"
+	case potatoes = "Potatoes"
+	case poutineries = "Poutineries"
+	case pubfood = "Pub Food"
+	case raw_food = "Live/Raw Food"
+	case riceshop = "Rice"
+	case romanian = "Romanian"
+	case rotisserie_chicken = "Rotisserie Chicken"
+	case russian = "Russian"
+	case salad = "Salad"
+	case sandwiches = "Sandwiches"
+	case scandinavian = "Scandinavian"
+	case schnitzel = "Schnitzel"
+	case scottish = "Scottish"
+	case seafood = "Seafood"
+	case serbocroatian = "Serbo Croatian"
+	case signature_cuisine = "Signature Cuisine"
+	case singaporean = "Singaporean"
+	case slovakian = "Slovakian"
+	case somali = "Somali"
+	case soulfood = "Soul Food"
+	case soup = "Soup"
+	case southern = "Southern"
+	case spanish = "Spanish"
+	case srilankan = "Sri Lankan"
+	case steak = "Steakhouses"
+	case sud_ouest = "French Southwest"
+	case supperclubs = "Supper Clubs"
+	case sushi = "Sushi Bars"
+	case swabian = "Swabian"
+	case swedish = "Swedish"
+	case swissfood = "Swiss Food"
+	case syrian = "Syrian"
+	case tabernas = "Tabernas"
+	case taiwanese = "Taiwanese"
+	case tapas = "Tapas Bars"
+	case tapasmallplates = "Tapas/Small Plates"
+	case tavolacalda = "Tavola Calda"
+	case texmex = "Tex-Mex"
+	case thai = "Thai"
+	case tradamerican = "American (Traditional)"
+	case traditional_swedish = "Traditional Swedish"
+	case trattorie = "Trattorie"
+	case turkish = "Turkish"
+	case ukrainian = "Ukrainian"
+	case uzbek = "Uzbek"
+	case vegan = "Vegan"
+	case vegetarian = "Vegetarian"
+	case venison = "Venison"
+	case vietnamese = "Vietnamese"
+	case waffles = "Waffles"
+	case wok = "Wok"
+	case wraps = "Wraps"
+	case yugoslav = "Yugoslav"
+
+	var alias: String {
+		switch self {
+		case .afghani:
+			return "afghani"
+		case .african:
+			return "african"
+		case .andalusian:
+			return "andalusian"
+		case .arabian:
+			return "arabian"
+		case .argentine:
+			return "argentine"
+		case .armenian:
+			return "armenian"
+		case .asianfusion:
+			return "asianfusion"
+		case .asturian:
+			return "asturian"
+		case .australian:
+			return "australian"
+		case .austrian:
+			return "austrian"
+		case .baguettes:
+			return "baguettes"
+		case .bangladeshi:
+			return "bangladeshi"
+		case .basque:
+			return "basque"
+		case .bavarian:
+			return "bavarian"
+		case .bbq:
+			return "bbq"
+		case .beergarden:
+			return "beergarden"
+		case .beerhall:
+			return "beerhall"
+		case .beisl:
+			return "beisl"
+		case .belgian:
+			return "belgian"
+		case .bistros:
+			return "bistros"
+		case .blacksea:
+			return "blacksea"
+		case .brasseries:
+			return "brasseries"
+		case .brazilian:
+			return "brazilian"
+		case .breakfast_brunch:
+			return "breakfast_brunch"
+		case .british:
+			return "british"
+		case .buffets:
+			return "buffets"
+		case .bulgarian:
+			return "bulgarian"
+		case .burgers:
+			return "burgers"
+		case .burmese:
+			return "burmese"
+		case .cafes:
+			return "cafes"
+		case .cafeteria:
+			return "cafeteria"
+		case .cajun:
+			return "cajun"
+		case .cambodian:
+			return "cambodian"
+		case .canteen:
+			return "canteen"
+		case .caribbean:
+			return "caribbean"
+		case .catalan:
+			return "catalan"
+		case .cheesesteaks:
+			return "cheesesteaks"
+		case .chicken_wings:
+			return "chicken_wings"
+		case .chickenshop:
+			return "chickenshop"
+		case .chilean:
+			return "chilean"
+		case .chinese:
+			return "chinese"
+		case .comfortfood:
+			return "comfortfood"
+		case .corsican:
+			return "corsican"
+		case .creperies:
+			return "creperies"
+		case .cuban:
+			return "cuban"
+		case .currysausage:
+			return "currysausage"
+		case .cypriot:
+			return "cypriot"
+		case .czech:
+			return "czech"
+		case .czechslovakian:
+			return "czechslovakian"
+		case .danish:
+			return "danish"
+		case .delis:
+			return "delis"
+		case .diners:
+			return "diners"
+		case .dinnertheater:
+			return "dinnertheater"
+		case .dumplings:
+			return "dumplings"
+		case .eastern_european:
+			return "eastern_european"
+		case .eltern_cafes:
+			return "eltern_cafes"
+		case .eritrean:
+			return "eritrean"
+		case .ethiopian:
+			return "ethiopian"
+		case .filipino:
+			return "filipino"
+		case .fischbroetchen:
+			return "fischbroetchen"
+		case .fishnchips:
+			return "fishnchips"
+		case .flatbread:
+			return "flatbread"
+		case .fondue:
+			return "fondue"
+		case .food_court:
+			return "food_court"
+		case .foodstands:
+			return "foodstands"
+		case .freiduria:
+			return "freiduria"
+		case .french:
+			return "french"
+		case .galician:
+			return "galician"
+		case .gamemeat:
+			return "gamemeat"
+		case .gastropubs:
+			return "gastropubs"
+		case .georgian:
+			return "georgian"
+		case .german:
+			return "german"
+		case .giblets:
+			return "giblets"
+		case .gluten_free:
+			return "gluten_free"
+		case .greek:
+			return "greek"
+		case .guamanian:
+			return "guamanian"
+		case .halal:
+			return "halal"
+		case .hawaiian:
+			return "hawaiian"
+		case .heuriger:
+			return "heuriger"
+		case .himalayan:
+			return "himalayan"
+		case .hkcafe:
+			return "hkcafe"
+		case .honduran:
+			return "honduran"
+		case .hotdog:
+			return "hotdog"
+		case .hotdogs:
+			return "hotdogs"
+		case .hotpot:
+			return "hotpot"
+		case .hungarian:
+			return "hungarian"
+		case .iberian:
+			return "iberian"
+		case .indonesian:
+			return "indonesian"
+		case .indpak:
+			return "indpak"
+		case .international:
+			return "international"
+		case .irish:
+			return "irish"
+		case .island_pub:
+			return "island_pub"
+		case .israeli:
+			return "israeli"
+		case .italian:
+			return "italian"
+		case .japanese:
+			return "japanese"
+		case .jewish:
+			return "jewish"
+		case .kebab:
+			return "kebab"
+		case .kopitiam:
+			return "kopitiam"
+		case .korean:
+			return "korean"
+		case .kosher:
+			return "kosher"
+		case .kurdish:
+			return "kurdish"
+		case .laos:
+			return "laos"
+		case .laotian:
+			return "laotian"
+		case .latin:
+			return "latin"
+		case .lyonnais:
+			return "lyonnais"
+		case .malaysian:
+			return "malaysian"
+		case .meatballs:
+			return "meatballs"
+		case .mediterranean:
+			return "mediterranean"
+		case .mexican:
+			return "mexican"
+		case .mideastern:
+			return "mideastern"
+		case .milkbars:
+			return "milkbars"
+		case .modern_australian:
+			return "modern_australian"
+		case .modern_european:
+			return "modern_european"
+		case .mongolian:
+			return "mongolian"
+		case .moroccan:
+			return "moroccan"
+		case .newamerican:
+			return "newamerican"
+		case .newcanadian:
+			return "newcanadian"
+		case .newmexican:
+			return "newmexican"
+		case .newzealand:
+			return "newzealand"
+		case .nicaraguan:
+			return "nicaraguan"
+		case .nightfood:
+			return "nightfood"
+		case .nikkei:
+			return "nikkei"
+		case .noodles:
+			return "noodles"
+		case .norcinerie:
+			return "norcinerie"
+		case .norwegian:
+			return "norwegian"
+		case .opensandwiches:
+			return "opensandwiches"
+		case .oriental:
+			return "oriental"
+		case .pakistani:
+			return "pakistani"
+		case .panasian:
+			return "panasian"
+		case .parma:
+			return "parma"
+		case .persian:
+			return "persian"
+		case .peruvian:
+			return "peruvian"
+		case .pfcomercial:
+			return "pfcomercial"
+		case .pita:
+			return "pita"
+		case .pizza:
+			return "pizza"
+		case .polish:
+			return "polish"
+		case .polynesian:
+			return "polynesian"
+		case .popuprestaurants:
+			return "popuprestaurants"
+		case .portuguese:
+			return "portuguese"
+		case .potatoes:
+			return "potatoes"
+		case .poutineries:
+			return "poutineries"
+		case .pubfood:
+			return "pubfood"
+		case .raw_food:
+			return "raw_food"
+		case .riceshop:
+			return "riceshop"
+		case .romanian:
+			return "romanian"
+		case .rotisserie_chicken:
+			return "rotisserie_chicken"
+		case .russian:
+			return "russian"
+		case .salad:
+			return "salad"
+		case .sandwiches:
+			return "sandwiches"
+		case .scandinavian:
+			return "scandinavian"
+		case .schnitzel:
+			return "schnitzel"
+		case .scottish:
+			return "scottish"
+		case .seafood:
+			return "seafood"
+		case .serbocroatian:
+			return "serbocroatian"
+		case .signature_cuisine:
+			return "signature_cuisine"
+		case .singaporean:
+			return "singaporean"
+		case .slovakian:
+			return "slovakian"
+		case .somali:
+			return "somali"
+		case .soulfood:
+			return "soulfood"
+		case .soup:
+			return "soup"
+		case .southern:
+			return "southern"
+		case .spanish:
+			return "spanish"
+		case .srilankan:
+			return "srilankan"
+		case .steak:
+			return "steak"
+		case .sud_ouest:
+			return "sud_ouest"
+		case .supperclubs:
+			return "supperclubs"
+		case .sushi:
+			return "sushi"
+		case .swabian:
+			return "swabian"
+		case .swedish:
+			return "swedish"
+		case .swissfood:
+			return "swissfood"
+		case .syrian:
+			return "syrian"
+		case .tabernas:
+			return "tabernas"
+		case .taiwanese:
+			return "taiwanese"
+		case .tapas:
+			return "tapas"
+		case .tapasmallplates:
+			return "tapasmallplates"
+		case .tavolacalda:
+			return "tavolacalda"
+		case .texmex:
+			return "tex-mex"
+		case .thai:
+			return "thai"
+		case .tradamerican:
+			return "tradamerican"
+		case .traditional_swedish:
+			return "traditional_swedish"
+		case .trattorie:
+			return "trattorie"
+		case .turkish:
+			return "turkish"
+		case .ukrainian:
+			return "ukrainian"
+		case .uzbek:
+			return "uzbek"
+		case .vegan:
+			return "vegan"
+		case .vegetarian:
+			return "vegetarian"
+		case .venison:
+			return "venison"
+		case .vietnamese:
+			return "vietnamese"
+		case .waffles:
+			return "waffles"
+		case .wok:
+			return "wok"
+		case .wraps:
+			return "wraps"
+		case .yugoslav:
+			return "yugoslav"
+		}
+	}
+}
